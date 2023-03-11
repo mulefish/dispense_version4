a33249460e93482f7d95b0e276d4829263fb0d81
pmontgomery <finch.wren.swift@gmail.com>
2023-03-11 11:40:16 -0800

    working it

--- static/bag_of_functions.js
+++ static/bag_of_functions.js
@@ -7,16 +7,19 @@ const possibleCommands = ["machine", "store", "spool", "keys"];
 function log_info(msg) {
     console.log("%c" + msg, "background:lightgreen;")
 }
+
 function log_error(msg) {
     console.log("%c" + msg, "background:lightred;")
 }
+
 function log_obj(obj) {
-    console.log( JSON.stringify( obj, null, 2 ))
+    console.log(JSON.stringify(obj, null, 2))
 }
 
 
 ////// Show the human if the data is well shaped or not 
 const colorDiv = document.getElementById('isItWellFormed');
+
 function indicateIsWellFormed(isOk_orIsNothing_orIsBad, issues) {
     switch (isOk_orIsNothing_orIsBad) {
         case true:
@@ -30,7 +33,7 @@ function indicateIsWellFormed(isOk_orIsNothing_orIsBad, issues) {
                 colorDiv.innerHTML = DATA_IS_GOOD
             }
             break;
-        case false: 
+        case false:
             if (colorDiv.classList.contains(DATA_NO_OPINION)) {
                 colorDiv.classList.remove(DATA_NO_OPINION);
                 colorDiv.classList.add(DATA_IS_BAD);
@@ -43,59 +46,51 @@ function indicateIsWellFormed(isOk_orIsNothing_orIsBad, issues) {
             break;
         default:
             if (colorDiv.classList.contains(DATA_IS_GOOD)) {
-            colorDiv.classList.remove(DATA_IS_GOOD);
-            colorDiv.classList.add(DATA_NO_OPINION);
-            colorDiv.innerHTML = ""
-        } else if (colorDiv.classList.contains(DATA_IS_BAD)) {
-            colorDiv.classList.remove(DATA_IS_BAD);
-            colorDiv.classList.add(DATA_NO_OPINION);
-            colorDiv.innerHTML = ""
-        }
+                colorDiv.classList.remove(DATA_IS_GOOD);
+                colorDiv.classList.add(DATA_NO_OPINION);
+                colorDiv.innerHTML = ""
+            } else if (colorDiv.classList.contains(DATA_IS_BAD)) {
+                colorDiv.classList.remove(DATA_IS_BAD);
+                colorDiv.classList.add(DATA_NO_OPINION);
+                colorDiv.innerHTML = ""
+            }
             break;
     }
-    // if (isOk_orIsNothing_orIsBad == undefined) {
-    //     if (colorDiv.classList.contains(DATA_IS_GOOD)) {
-    //         colorDiv.classList.remove(DATA_IS_GOOD);
-    //         colorDiv.classList.add(DATA_NO_OPINION);
-    //         colorDiv.innerHTML = ""
-    //     } else if (colorDiv.classList.contains(DATA_IS_BAD)) {
-    //         colorDiv.classList.remove(DATA_IS_BAD);
-    //         colorDiv.classList.add(DATA_NO_OPINION);
-    //         colorDiv.innerHTML = ""
-    //     }
-    // } else {
-    //     if (isOk_orIsNothing_orIsBad === true) {
-
-    //         if (colorDiv.classList.contains(DATA_NO_OPINION)) {
-    //             colorDiv.classList.remove(DATA_NO_OPINION);
-    //             colorDiv.classList.add(DATA_IS_GOOD);
-    //             colorDiv.innerHTML = DATA_IS_GOOD
-    //         } else if (colorDiv.classList.contains(DATA_IS_BAD)) {
-    //             colorDiv.classList.remove(DATA_IS_BAD);
-    //             colorDiv.classList.add(DATA_IS_GOOD);
-    //             colorDiv.innerHTML = DATA_IS_GOOD
-    //         }
-    //     } else if (isOk_orIsNothing_orIsBad === false) {
-    //         if (colorDiv.classList.contains(DATA_NO_OPINION)) {
-    //             colorDiv.classList.remove(DATA_NO_OPINION);
-    //             colorDiv.classList.add(DATA_IS_BAD);
-    //             colorDiv.innerHTML = issues
-    //         } else if (colorDiv.classList.contains(DATA_IS_GOOD)) {
-    //             colorDiv.classList.remove(DATA_IS_GOOD);
-    //             colorDiv.classList.add(DATA_IS_BAD);
-    //             colorDiv.innerHTML = issues
-    //         }
-    //     }
-    // }
 }
-//////////
-
-
 ///// Most important func /////////////////
-function fileUpload(event) { 
+function fileUpload(event) {
     function isValidCommand(candidate) {
         return possibleCommands.includes(candidate);
     }
+
+    function createTable(data, storeId, machineId, columns) { 
+        log_obj(data)
+        log_info( "storeId " + JSON.stringify( storeId ))
+        log_info( "machineId " + JSON.stringify( machineId ))
+        log_info( "columns " + JSON.stringify( columns ))
+
+        let table = "<table border='1' class='machineTable'><tr>"
+        columns.forEach((c, i)=> {
+            table += "<th>" + c + "</th>"
+        })
+        table += "</tr>"
+        data.forEach((row, i)=> {
+            table += "<tr>"
+            row.forEach((column)=> { 
+                table += "<td>" + column + "</td>"
+            })
+            table += "</tr>"
+        })
+        table += "</table>"
+        document.getElementById("machineTable").innerHTML = table 
+
+
+
+
+    }
+
+
+
     const file = event.target.files[0];
     const reader = new FileReader();
     reader.onload = (event) => {
@@ -109,36 +104,49 @@ function fileUpload(event) {
             header: 1
         });
         const data = []
-        const seen = {} 
-
+        const seen = {}
+        let storeId = []
+        let machineId = []
+        let columns = []
         rows.forEach((row, i) => {
             if (row.length > 0) {
                 const candidate = row[0].trim().toLowerCase()
                 if (isValidCommand(candidate)) {
-                    seen[candidate]=1
-                    data.push(row)
+                    seen[candidate] = 1
+                    if ( candidate === "store") {
+                        storeId = row
+                    } else if ( candidate === "keys") {
+                        columns = row
+                    } else if ( candidate === "machine") { 
+                        machine = row
+                    } else if ( candidate === "spool") {
+                        data.push(row)
+                    }
                 }
             }
         })
 
-        let issues = "" 
-        let isOk = true 
-        possibleCommands.forEach((cmd)=> { 
-            if ( ! seen.hasOwnProperty(cmd)) {
-                issues += cmd + " "
-                isOk = false 
+        let issues = ""
+        let isOk = true
+        possibleCommands.forEach((cmd) => {
+            if (!seen.hasOwnProperty(cmd)) {
+                issues += cmd + ","
+                isOk = false
             }
         })
-        indicateIsWellFormed(isOk, issues)
-        log_obj(seen )
+        if ( isOk === false ) { 
+            // snip the trailing comma
+            issues = issues.substring(0, issues.length - 1)
+            indicateIsWellFormed(isOk, "Missing " + issues)
+        } else {
+            createTable(data, storeId, machineId, columns)
+            indicateIsWellFormed(isOk)
+        }
+
+
+
+
     };
     reader.readAsBinaryString(file);
 };
-
-////////////////// THIS IS IMPORTANT! 
-const fileInput = document.getElementById('fileInput');
-fileInput.addEventListener('change', (event) => {
-     fileUpload(event)
-})
-
 
