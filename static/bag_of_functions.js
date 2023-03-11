88f162cf9803a4a5b5aae414e9be8a538df0b18a
pmontgomery <finch.wren.swift@gmail.com>
2023-03-11 11:04:29 -0800

    working it

--- static/bag_of_functions.js
+++ static/bag_of_functions.js
@@ -1,54 +1,133 @@
-const colorDiv = document.getElementById('isItWellFormed');
-function toggleColor() {
-  if (colorDiv.classList.contains('white')) {
-    colorDiv.classList.remove('white');
-    colorDiv.classList.add('red');
-    colorDiv.innerHTML = "NO"
-  } else if (colorDiv.classList.contains('red')) {
-    colorDiv.classList.remove('red');
-    colorDiv.classList.add('green');
-    colorDiv.innerHTML = "YES"
-  } else {
-    colorDiv.classList.remove('green');
-    colorDiv.classList.add('white');
-    colorDiv.innerHTML = ""
-  }
+const DATA_NO_OPINION = "no_opinion"
+const DATA_IS_GOOD = "is_good"
+const DATA_IS_BAD = "is_bad"
+const possibleCommands = ["machine", "store", "spool", "keys"];
+
+//// commmon funcs 
+function log_info(msg) {
+    console.log("%c" + msg, "background:lightgreen;")
+}
+function log_error(msg) {
+    console.log("%c" + msg, "background:lightred;")
+}
+function log_obj(obj) {
+    console.log( JSON.stringify( obj, null, 2 ))
 }
 
 
-function indicateIsWellFormed(isOk_orIsNothing_orIsBad) {
-    if ( isOk_orIsNothing_orIsBad === undefined ) {
-        if (colorDiv.classList.contains("green")) {
-            colorDiv.classList.remove("green");
-            colorDiv.classList.add("white");
-            colorDiv.innerHTML = "NOTHING"
-        } else if (colorDiv.classList.contains('red')) {
-            colorDiv.classList.remove('red');
-            colorDiv.classList.add('white');
-            colorDiv.innerHTML = "NOTHING"
+////// Show the human if the data is well shaped or not 
+const colorDiv = document.getElementById('isItWellFormed');
+function indicateIsWellFormed(isOk_orIsNothing_orIsBad, issues) {
+    console.log("IWF: " + isOk_orIsNothing_orIsBad)
+    if (isOk_orIsNothing_orIsBad == undefined) {
+        if (colorDiv.classList.contains(DATA_IS_GOOD)) {
+
+            console.log("A A")
+            colorDiv.classList.remove(DATA_IS_GOOD);
+            colorDiv.classList.add(DATA_NO_OPINION);
+            colorDiv.innerHTML = ""
+        } else if (colorDiv.classList.contains(DATA_IS_BAD)) {
+            console.log("A B")
+
+            colorDiv.classList.remove(DATA_IS_BAD);
+            colorDiv.classList.add(DATA_NO_OPINION);
+            colorDiv.innerHTML = ""
+        } else {
+            console.log("hello the classList is " + colorDiv.classList)
         }
     } else {
-        if (isOk_orIsNothing_orIsBad === true ) {
-            if (colorDiv.classList.contains('white')) {
-                colorDiv.classList.remove('white');
-                colorDiv.classList.add('green');
-                colorDiv.innerHTML = "YES"
-            } else if (colorDiv.classList.contains('red')) {
-                colorDiv.classList.remove('red');
-                colorDiv.classList.add('green');
-                colorDiv.innerHTML = "YES"
+        if (isOk_orIsNothing_orIsBad === true) {
+
+            if (colorDiv.classList.contains(DATA_NO_OPINION)) {
+                console.log("B A")
+                colorDiv.classList.remove(DATA_NO_OPINION);
+                colorDiv.classList.add(DATA_IS_GOOD);
+                colorDiv.innerHTML = DATA_IS_GOOD
+            } else if (colorDiv.classList.contains(DATA_IS_BAD)) {
+                console.log("B B")
+                colorDiv.classList.remove(DATA_IS_BAD);
+                colorDiv.classList.add(DATA_IS_GOOD);
+                colorDiv.innerHTML = DATA_IS_GOOD
             }
-        } else if ( isOk_orIsNothing_orIsBad === false ) { 
-            if (colorDiv.classList.contains('white')) {
-                colorDiv.classList.remove('white');
-                colorDiv.classList.add('red');
-                colorDiv.innerHTML = "NO"
-            } else if (colorDiv.classList.contains('green')) {
-                colorDiv.classList.remove('green');
-                colorDiv.classList.add('red');
-                colorDiv.innerHTML = "NO"
+        } else if (isOk_orIsNothing_orIsBad === false) {
+            if (colorDiv.classList.contains(DATA_NO_OPINION)) {
+                console.log("C A")
+
+                colorDiv.classList.remove(DATA_NO_OPINION);
+                colorDiv.classList.add(DATA_IS_BAD);
+                colorDiv.innerHTML = issues
+            } else if (colorDiv.classList.contains(DATA_IS_GOOD)) {
+                console.log("C B")
+
+                colorDiv.classList.remove(DATA_IS_GOOD);
+                colorDiv.classList.add(DATA_IS_BAD);
+                colorDiv.innerHTML = issues
             }
         }
-     }
-  }
-  
+    }
+}
+//////////
+
+
+///// Most important func /////////////////
+function fileUpload(event) { 
+    function isValidCommand(candidate) {
+        return possibleCommands.includes(candidate);
+    }
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (event) => {
+        const fileData = event.target.result;
+        const workbook = XLSX.read(fileData, {
+            type: 'binary'
+        });
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+        const rows = XLSX.utils.sheet_to_json(worksheet, {
+            header: 1
+        });
+        const data = []
+        const seen = {} 
+
+        rows.forEach((row, i) => {
+            if (row.length > 0) {
+                const candidate = row[0].trim().toLowerCase()
+                if (isValidCommand(candidate)) {
+                    seen[candidate]=1
+                    data.push(row)
+                }
+            }
+        })
+
+        let issues = "" 
+        let isOk = true 
+        possibleCommands.forEach((cmd)=> { 
+            if ( ! seen.hasOwnProperty(cmd)) {
+                issues += cmd + " "
+                isOk = false 
+            }
+        })
+        indicateIsWellFormed(isOk, issues)
+        log_obj(seen )
+    };
+    reader.readAsBinaryString(file);
+    // function dataIsOkSoFar(data) {
+    //     data.each()
+
+
+
+    // }
+};
+
+
+
+
+
+
+
+const fileInput = document.getElementById('fileInput');
+fileInput.addEventListener('change', (event) => {
+     fileUpload(event)
+})
+
+
--- tdd.js
+++ tdd.js
@@ -1,3 +1,28 @@
-const a = "         HelloO there ! "
-const cmd = a.trim().toLowerCase() 
-console.log( "|" + cmd + "|") 
+const possibleCommands = ["machine", "store", "spool", "keys"];
+
+const seen = {} 
+
+
+const allKeysHaveValueOne = possibleCommands.every(key => {
+    const value = seen[key];
+    return value === 1;
+  });
+
+possibleCommands.forEach((cmd)=> { 
+    seen[cmd] = 1
+})
+
+console.log(  allKeysHaveValueOne) 
+
+
+const allKeysHaveValueOne1 = possibleCommands.every(key => {
+    const value = seen[key];
+    return value === 1;
+  });
+
+  console.log(  allKeysHaveValueOne1) 
+
+
+
+seen["store"]=0
+// console.log( allKeysHaveValueOne(seen)) 
