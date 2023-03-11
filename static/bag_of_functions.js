13418879bdac5ef529a724146a82dca73ea63890
pmontgomery <finch.wren.swift@gmail.com>
2023-03-11 10:17:20 -0800

    working it

--- static/bag_of_functions.js
+++ static/bag_of_functions.js
@@ -12,7 +12,43 @@ function toggleColor() {
     colorDiv.classList.remove('green');
     colorDiv.classList.add('white');
     colorDiv.innerHTML = ""
-
   }
-
 }
+
+
+function indicateIsWellFormed(isOk_orIsNothing_orIsBad) {
+    if ( isOk_orIsNothing_orIsBad === undefined ) {
+        if (colorDiv.classList.contains("green")) {
+            colorDiv.classList.remove("green");
+            colorDiv.classList.add("white");
+            colorDiv.innerHTML = "NOTHING"
+        } else if (colorDiv.classList.contains('red')) {
+            colorDiv.classList.remove('red');
+            colorDiv.classList.add('white');
+            colorDiv.innerHTML = "NOTHING"
+        }
+    } else {
+        if (isOk_orIsNothing_orIsBad === true ) {
+            if (colorDiv.classList.contains('white')) {
+                colorDiv.classList.remove('white');
+                colorDiv.classList.add('green');
+                colorDiv.innerHTML = "YES"
+            } else if (colorDiv.classList.contains('red')) {
+                colorDiv.classList.remove('red');
+                colorDiv.classList.add('green');
+                colorDiv.innerHTML = "YES"
+            }
+        } else if ( isOk_orIsNothing_orIsBad === false ) { 
+            if (colorDiv.classList.contains('white')) {
+                colorDiv.classList.remove('white');
+                colorDiv.classList.add('red');
+                colorDiv.innerHTML = "NO"
+            } else if (colorDiv.classList.contains('green')) {
+                colorDiv.classList.remove('green');
+                colorDiv.classList.add('red');
+                colorDiv.innerHTML = "NO"
+            }
+        }
+     }
+  }
+  
