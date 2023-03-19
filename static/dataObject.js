622383cfdae0cff7693974ac33407bc9a6a1c051
pmontgomery <finch.wren.swift@gmail.com>
2023-03-18 21:13:19 -0700

    here we go

--- static/dataObject.js
+++ static/dataObject.js
@@ -1,42 +1,51 @@
 
-class DataObject { 
+class DataObject {
 
-    constructor() { 
-        this.isTesting = false 
+    constructor() {
+        this.isTesting = false
         this.mandatoryColumns = ["spool", "uid", "count", "price"];
         this.NILL = "NILL"
-        this.spools = [] 
-        this.health = [] 
-        this.columns = {} 
+        this.spools = []
+        this.health = []
+        this.columns = {}
         this.storeId = this.NILL
-        this.machineId = this.NILL 
-        this.errors = {
-        }
-        this.health = [] 
-        this.storeId_health="ok"
+        this.machineId = this.NILL
+        this.errors = {}
+        this.health = []
+        this.storeId_health = "ok"
         this.machineId_health = "ok"
         this.idReg_oneLetter_oneNumber = /^[A-Za-z][0-9]$/;
     }
-    getSpools() { 
+    getSpools() {
         let LoH = []
-        this.spools.forEach((row, i)=> { 
-            if ( i === 0 ) {
+        this.spools.forEach((row, i) => {
+            if (i === 0) {
                 // Skip it! This is just the 
             }
             let mandatory = {}
-            let optional = {} 
-            for ( let k in this.columns) {
-                    const index = this.columns[k]
-                    const v = row[index]
-                    if ( this.mandatoryColumns.includes(k)) {
-                        mandatory[k] = v
+            let optional = {}
+            for (let k in this.columns) {
+                const index = this.columns[k]
+                const v = row[index]
+                if (this.mandatoryColumns.includes(k)) {
+                    mandatory[k] = v
+                } else {
+
+                    if (k.toLowerCase() === "harvest") {
+
+                        const pretty_date = this.getDate_fromExcelSerialDate(v)
+                        optional[k] = pretty_date
+
                     } else {
                         optional[k] = v
+
                     }
+
+
+                }
             }
-            LoH.push({mandatory, optional})  
+            LoH.push({ mandatory, optional })
         })
-        //console.log( LoH)
         return LoH
     }
     addSpool(row) {
@@ -44,17 +53,17 @@ class DataObject {
         this.spools.push(row)
     }
     setMachine(machineId) {
-        if ( machineId !== undefined ) { // && machineId.length > 0 ) {         
+        if (machineId !== undefined) { // && machineId.length > 0 ) {         
             this.machineId = machineId
         }
     }
     setStore(storeId) {
-        if ( storeId !== undefined && storeId.length > 0 ) { 
+        if (storeId !== undefined && storeId.length > 0) {
             this.storeId = storeId
         }
     }
     setKeys(columns) {
-        columns.forEach((c, i)=>{
+        columns.forEach((c, i) => {
             const k = c.trim().toLowerCase()
             this.columns[k] = i
         })
@@ -63,72 +72,71 @@ class DataObject {
 
         try {
             const candidate = row[0].trim().toLowerCase()
-            if ( candidate === "spool") {
+            if (candidate === "spool") {
                 this.addSpool(row)
             }
 
-            if ( candidate === "machine") {
+            if (candidate === "machine") {
                 this.setMachine(row[1])
             }
 
-            if ( candidate === "store") {
+            if (candidate === "store") {
                 this.setStore(row[1])
             }
-            if ( candidate === "keys") {
+            if (candidate === "keys") {
                 this.setKeys(row)
             }
-        } catch ( rowIsIllFormed ) {
-            const msg = rowIsIllFormed.message 
-            if ( msg.includes("Cannot read properties of undefined")) {
+        } catch (rowIsIllFormed) {
+            const msg = rowIsIllFormed.message
+            if (msg.includes("Cannot read properties of undefined")) {
                 // ignore it
             } else {
-                if ( this.isTesting === false ) {
+                if (this.isTesting === false) {
                     console.log(rowIsIllFormed.message + "   |" + row + "|")
                 }
             }
         }
     }
 
-    _checkTheShape() { 
+    _checkTheShape() {
         // Did we get a store id? 
-        if ( this.storeId !== this.NILL && this.storeId.length > 0 ) {
+        if (this.storeId !== this.NILL && this.storeId.length > 0) {
         } else {
             this.errors["storeId"] = "is missing"
             this.storeId_health = this.NILL
         }
         // Did we get a machine id? 
-        if ( this.machineId !== this.NILL ) { // } && this.machineId.length > 0 ) {
+        if (this.machineId !== this.NILL) { // } && this.machineId.length > 0 ) {
         } else {
             this.errors["machineId"] = "is missing"
             this.machineId_health = this.NILL
         }
         // Did we get the minimum of the proper keys? 
-        this.mandatoryColumns.forEach((col) => { 
-            if ( ! this.columns.hasOwnProperty(col)) {
+        this.mandatoryColumns.forEach((col) => {
+            if (!this.columns.hasOwnProperty(col)) {
                 this.errors[col] = "is missing"
             }
         })
         // Do all the spools have the minimum of the proper values AND is the spool ID reasonible? 
-        let isFine = true 
-        let dataErrors = {} 
-        // const idReg_oneLetter_oneNumber = /^[A-Za-z][0-9]$/;
-        const spoolIds_dupeCheck = {} 
-        this.spools.forEach((spool, j)=> {     
+        let isFine = true
+        let dataErrors = {}
+        const spoolIds_dupeCheck = {}
+
+        this.spools.forEach((spool, j) => {
             this.health.push([])
-             Object.keys(this.columns).forEach((x, index)=> { 
+            Object.keys(this.columns).forEach((x, index) => {
                 this.health[j][index] = "ok"
             })
 
             const spoolId_index = this.columns["spool"]
             const price_index = this.columns["price"]
-            const count_index= this.columns["count"]
+            const count_index = this.columns["count"]
             const uid_index = this.columns["uid"]
             // Dates like 01/01/23 are converted into a seriel date like '44937'. 
             // getDate_fromExcelSerialDate will convert 44937 back to human friendly date 
             const harvest_index = this.columns["harvest"]
-            const pretty_date = this.getDate_fromExcelSerialDate( spool[harvest_index])
+            const pretty_date = this.getDate_fromExcelSerialDate(spool[harvest_index])
             spool[harvest_index] = pretty_date
-
             const spoolId = spool[spoolId_index]
             const price = spool[price_index]
             const count = spool[count_index]
@@ -136,9 +144,9 @@ class DataObject {
 
 
             // spools - See if missing OR if duplicated
-            if ( spoolId !== undefined && this.idReg_oneLetter_oneNumber.test(spoolId)) {
+            if (spoolId !== undefined && this.idReg_oneLetter_oneNumber.test(spoolId)) {
                 // spoolId is well shaped! Something like 'B5' 
-                if ( ! spoolIds_dupeCheck.hasOwnProperty(spoolId)) {
+                if (!spoolIds_dupeCheck.hasOwnProperty(spoolId)) {
                     spoolIds_dupeCheck[spoolId] = 1
                 } else {
                     spoolIds_dupeCheck[spoolId] += 1
@@ -150,9 +158,9 @@ class DataObject {
                 this.health[j][spoolId_index] = this.NILL
             }
             // prices - See if missing OR if not a whole number
-            if ( price !== undefined ) {
+            if (price !== undefined) {
                 const num = Number.parseInt(price);
-                if ( Number.isInteger(num) === true ) { 
+                if (Number.isInteger(num) === true) {
                     // GOOD!
                 } else {
                     dataErrors["price" + price] = "Price " + price + " is not a number"
@@ -163,9 +171,9 @@ class DataObject {
                 dataErrors["price"] = "A price is missing"
             }
             // count - See if missing OR if not a whole number
-            if ( count !== undefined ) {
+            if (count !== undefined) {
                 const num = Number.parseInt(count);
-                if ( Number.isInteger(num) === true ) { 
+                if (Number.isInteger(num) === true) {
                     // GOOD!
                 } else {
                     dataErrors["count" + count] = "Count '" + count + "' is not a number"
@@ -176,7 +184,7 @@ class DataObject {
                 this.health[j][count_index] = this.NILL
             }
             // uid - See if missing or empty
-            if ( uid !== undefined ) {
+            if (uid !== undefined) {
                 // GOOD!
             } else {
                 dataErrors["uid"] = "A uid is missing"
@@ -184,18 +192,18 @@ class DataObject {
             }
         });
 
-        for ( let k in dataErrors) { 
+        for (let k in dataErrors) {
             this.errors[k] = dataErrors[k]
         }
         return this.errors
     }
 
-    checkTheShape() { 
+    checkTheShape() {
         const theErrors = this._checkTheShape()
-        const n = Object.keys(theErrors).length 
+        const n = Object.keys(theErrors).length
         const result = {
-            isOk : n === 0 ? true : false,
-            errors:theErrors
+            isOk: n === 0 ? true : false,
+            errors: theErrors
         }
         return result
     }
@@ -205,32 +213,32 @@ class DataObject {
         // getDate_fromExcelSerialDate will convert 44937 back to human friendly date 
         function isValidDate(d) {
             return d instanceof Date && !isNaN(d);
-          }
-          function zeroPad(n) {
+        }
+        function zeroPad(n) {
             return n.toString().padStart(2, '0');
-          }
-          
-          function formatDate(date) {
+        }
+
+        function formatDate(date) {
             const month = zeroPad(date.getMonth() + 1);
             const day = zeroPad(date.getDate());
             const year = zeroPad(date.getFullYear() % 100);
             return `${month}/${day}/${year}`;
-          }
-    
+        }
+
         const unixTimestamp = (serialDate - 25569) * 86400000;
         const d = new Date(unixTimestamp);
-        if ( isValidDate(d)) {
+        if (isValidDate(d)) {
             let prettyDate = formatDate(d)
             return prettyDate
         } else {
             return d // it will say 'Invalid date'
         }
     }
-    
+
 }
 try {
     module.exports = DataObject
-} catch( ignore ) {
-        // This is here for TDD from the CLI. In a browser this would throw an error!
-        // Ignore it! 
+} catch (ignore) {
+    // This is here for TDD from the CLI. In a browser this would throw an error!
+    // Ignore it! 
 }
--- static/tdd.js
+++ static/tdd.js
@@ -39,8 +39,6 @@ function dataObject_populateProperty_HAPPYPATH() {
     excel.forEach((row)=> { 
         dataObject.addInfo(row)
     })
-    const x = dataObject.getSpools() 
-    console.log(x )
     const shape = dataObject.checkTheShape()
     verify(shape["isOk"], true, "dataObject_populateProperty_HAPPYPATH")
 }
@@ -251,16 +249,47 @@ function dataObject_getSpools_whichIsWhatIsSubmittedToBackend() {
           strain: 'Green Arrow',
           classification: 'Sativa',
           batchinfo: 'KLT - 685115300',
-          harvest: 'dinoblank',
+          harvest: '01/05/23',
           canabinoids: 21,
           'thc-a': 4,
           delta_9: 9,
           cbd: 5
         }
       }
-      console.log( actual )
+      //console.log( actual )
       verify(actual, expected, "dataObject_getSpools_whichIsWhatIsSubmittedToBackend")
 }
+
+
+/**
+ * Harvest has a complex lifecycle
+ * Honestly, I sort of forget it...
+ * BUT! 
+ * Part of it is 'what gets to the web page'
+ * and another part of it is 'what gets sent to the backend'. 
+ * Now, these two things ought to be the same but...  ...it is not the same. 
+ * This is a test to make sure things are synced up. 
+ * 
+ * TODO: Make 'syncing up' unneeded!  
+*/
+function dataObject_harvest() {
+    const dataObject = new DataObject() 
+    const excel = [
+        // See? Some slop is OK -  undefined to out of place things will be ignored
+        ["store", "Test", undefined, undefined, "ignore"],
+        ["machine", "888"], // The last machine will be picked up  
+        ["machine", "999"],
+        ['keys', 'spool', 'uid', 'count', ' price', 'Type', 'grams', 'Producer', 'Strain', 'Classification', 'BatchInfo', 'Harvest', 'Canabinoids', 'THC-A', 'Delta_9', 'CBD', "COLUMN_NAME_THIS_DOES_NOT_MATTER"],
+        ['This is not a key', 'blah blah'],
+        ['SPOOL', 'A1', '15643151 - A1', 9, 25, 'Flower', 3.5, 'Paddle Creek Cannabis', 'Green Arrow', 'Sativa', 'KLT - 685115300', 44932, 21, 4, 9, 5, "THIS WILL BE IGNORED"],
+    ]
+    excel.forEach((row)=> { 
+        dataObject.addInfo(row)
+    })
+    const shape = dataObject.checkTheShape()
+    verify(shape["isOk"], true, "dataObject_populateProperty_HAPPYPATH")
+}
+
 
 /**
 * Test runner
@@ -275,5 +304,7 @@ if (require.main === module) {
     dataObject_populateProperty_SADPATH_notLeftJustified()
     dataObject_populateProperty_SADPATH_spoolIdIsInformed()
     dataObject_regex_oneLetterOneNumber_spoolId() 
-//    dataObject_getSpools_whichIsWhatIsSubmittedToBackend()
+    dataObject_getSpools_whichIsWhatIsSubmittedToBackend()
+    dataObject_harvest()
 }
+
