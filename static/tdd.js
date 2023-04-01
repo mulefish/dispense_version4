b899438734a6369e1f284e533b6b0ec0b19df5e0
pmontgomery <finch.wren.swift@gmail.com>
2023-04-01 12:37:31 -0700

    Looks good: Now for the descripts_uid table

--- static/tdd.js
+++ static/tdd.js
@@ -270,26 +270,9 @@ function dataObject_getSpools_whichIsWhatIsSubmittedToBackend() {
         dataObject.addInfo(row)
     })
     const LoH = dataObject.getSpools() 
-    const actual = LoH[0]
-    const expected = {
-        mandatory: { spool: 'A1', uid: '15643151 - A1', count: 9, price: 25 },
-        optional: {
-          keys: 'SPoOL',
-          type: 'Flower',
-          grams: 3.5,
-          producer: 'Paddle Creek Cannabis',
-          strain: 'Green Arrow',
-          classification: 'Sativa',
-          batchinfo: 'KLT - 685115300',
-          harvest: '01/05/23',
-          canabinoids: 21,
-          'thc-a': 4,
-          delta_9: 9,
-          cbd: 5
-        }
-      }
-      console.log( actual )
-      verify(actual, expected, "dataObject_getSpools_whichIsWhatIsSubmittedToBackend")
+    actual = [LoH.length, Object.keys(LoH[0]).length]
+    expected = [3,2]
+    verify(actual, expected, "dataObject_getSpools_whichIsWhatIsSubmittedToBackend")
 }
 
 /**
